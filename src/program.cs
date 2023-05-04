0cc079d401d66a5a78a38c928e0f7769ca4d11b3
Santiago Mendoza <38741779+Pro0072006@users.noreply.github.com>
2023-05-04 13:58:29 -0500

    Create program.cs

---
+++ src/program.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace MateMa
+{
+    class Principal
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Thanks for downloanding my app");
+            Console.WriteLine("Por: Santiago Mendoza");
+        }
+    }
+}
